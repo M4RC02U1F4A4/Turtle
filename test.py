8dd9d1ca9ce88cd69efe2f79763119a21744e2cb
Marco <galmar97@gmail.com>
2018-03-29 12:43:06 +0200

    Test

--- test.py
+++ test.py
@@ -3,22 +3,40 @@ import random
 import time
 from random import randint as rand
 
+bg_color = "black"
 ob = turtle.Turtle()
-ob.speed(0)
+
+ob.speed(10)
 turtle.colormode(255)
 ob.hideturtle()
-k=15
-ob.pensize(k)
+ob.pensize(2)
+turtle.bgcolor(bg_color)
 
-for i in range(2000):
-    r = rand(0, 255)
-    g = rand(0, 255)
-    b = rand(0, 255)
-    ob.color(r, g, b)
-    if i%200 == 0:
-        k += 1
-        ob.pensize(k)
-    ob.forward(0 + i)
-    ob.left(91)
+turtle.tracer(0, 0)
 
+rainbowColors = ["#FF0000","#FFA600","#FFFF00", "#62FF00", "#1E56FC","#4800FF","#CC00FF","#69C5FF"]
+k=0
+
+ob.goto(0, 0)
+for j in range(360):
+    ob.color(rainbowColors[rand(0,7)])
+    ob.forward(100)
+    ob.color(bg_color)
+    ob.forward(50)
+    ob.color(rainbowColors[rand(0,7)])
+    ob.forward(100)
+    ob.color(bg_color)
+    ob.forward(50)
+    ob.color(rainbowColors[rand(0,7)])
+    ob.forward(100)
+    ob.penup()
+    ob.goto(0,0)
+    ob.pendown()
+    ob.left(1)
+
+
+
+
+
+turtle.update()
 turtle.done()
