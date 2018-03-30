502fa6ffced87c5f9a3c1dcd2c82d9ee419c50d5
Marco <galmar97@gmail.com>
2018-03-30 15:09:18 +0200

    test

--- test.py
+++ test.py
@@ -1,89 +1,49 @@
 import turtle
 import random
-import time
 from random import randint as rand
 import cmath
 
-def collega(x_p, y_p, ob, x, y):
-    r, g, b = 0, 255, 0
-    for i in range(int(len(x)/2)):
-        if(r > 0 and b == 0):
-            r -= 1
-            g += 1
-        if(g > 0 and r == 0):
-            g -= 1
-            b += 1
-        if(b > 0 and g == 0):
-            r += 1
-            b -= 1
-        ob.color(r, g, b)
-        ob.penup()
-        ob.goto(x_p, y_p)
-        ob.pendown()
-        ob.goto(x[i], y[i])
-    r, g, b = 0, 255, 0
-    i = len(x)-1
-    while(i>=len(x)/2):
-        if(r > 0 and b == 0):
-            r -= 1
-            g += 1
-        if(g > 0 and r == 0):
-            g -= 1
-            b += 1
-        if(b > 0 and g == 0):
-            r += 1
-            b -= 1
-        ob.color(r, g, b)
-        ob.penup()
-        ob.goto(x_p, y_p)
-        ob.pendown()
-        ob.goto(x[i], y[i])
-        i -= 1
 
 
 bg_color = "black"
 ob = turtle.Turtle()
 
-ob.speed(0)
+ob.speed(10)
 turtle.colormode(255)
 ob.hideturtle()
-ob.pensize(2)
+ob.pensize(1)
 turtle.bgcolor(bg_color)
-ob.color("black")
+ob.color("white")
 
 turtle.tracer(0, 0)
-x1 = []
-y1 = []
-x2 = []
-y2 = []
-k=-400
-while(k<400):
-    #r = rand(0, 360)
-    x1.append(2*k)
-    y1.append(50*cmath.cos(k/12).real-700)
-    k += 1.1
 
-k=-400
-while(k<400):
-    #r = rand(0, 360)
-    x2.append(2*k)
-    y2.append(-50*cmath.cos(k/12).real+700)
-    k += 1.1
+x = []
+y = []
 
-ob.penup()
-ob.goto(x1[0], y1[0])
-ob.pendown()
-for i in range(len(x1)):
-    ob.goto(x1[i], y1[i])
+k = -500
+z = -500
+a = -500
+while(k < 500):
+    while(z < 500):
+        while(a < 500):
+            x.append(k)
+            y.append(cmath.sqrt((-1)*(1-a**2-k**2-z**2)).real)
+            a += 10
+        a = -500
+        z += 10
+    z = -500
+    k += 10
+
+print(x, y)
 
 ob.penup()
-ob.goto(x2[0], y2[0])
+ob.goto(x[0], y[0])
 ob.pendown()
-for i in range(len(x1)):
-    ob.goto(x2[i], y2[i])
+for i in range(len(x)):
+    ob.goto(x[i], y[i]-400)
+    print(len(x)-i)
 
-collega(0, 400, ob, x1, y1)
-collega(0, -400, ob, x2, y2)
+
 
 turtle.update()
 turtle.done()
